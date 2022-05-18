634c7272d6ff6ca72cd28d8346c3459e33d89145
Roberto Molowny-Horas <roberto@creaf.uab.es>
2022-05-18 19:16:52 +0200

    Silly problems with github. No changes.

--- R/simu_deaths.r
+++ R/simu_deaths.r
@@ -1,54 +1,43 @@
 #' Simulation of survival data set
 #'
-#' It simulates a data set of right-censored n individuals for a
-#' given survival function.
+#' It simulates one right-censored data set by randomly drawing n individuals from
+#' a given survival function.
 #'
 #' @param time time points at which survival is evaluated.
 #' @param prob survival probability density function.
-#' @param n number of individuals to simulate.
+#' @param n_ind number of individuals to simulate.
 #'
-#' @details
-#' To be completed.
+#' @details \code{n_ind} probabilities are drawn at random. The survival function has to be
+#' monotonically decreasing.
+#' That is, if \code{f(x)} is the survival function, \code{f(x)>=f(y)} for \code{x>=y}.
 #'
-#' @return \code{data.frame} with two columns named \code{Death} and \code{Event}.
+#' @return \code{data.frame} with \code{n_ind} rows and two columns
+#' named \code{Death} and \code{Event}.
 #' The former yields the \code{time} of the event and the latter is the status
-#' indicates, i.e. dead (\code{Event=1}) or alive (i.e. \code{Event=0}).
+#' indicator, i.e. dead (\code{Event=1}) or alive (i.e. \code{Event=0}).
 #' 
 #' @examples
 #'
-#' ## Simulating one random sample data from the survival function.
+#' ##
 #' time <- 0:100
 #' prob <- exp(-time*.05)
-#' n <- 10
-#' x <- simu_deaths(time,prob,n)
+#' n_ind <- 120
+#' x <- simu_deaths(time,prob,n_ind)
 #' 
-#' ## Inverse approach: approximating the survival function from simulations.
-#' ## We build a more complicated survival function, one which is not sampled
-#' ## regularly and has flat parts.
-#' time <- c(1,3,4,8,9,12:17)
-#' prob <- c(1,1,1,.9,.8,.8,.7,.5,.2,.2,.2)
-#' n <- 10
+#' ## Simulating the original survival function.
 #' nrep <- 100
-#' y <- NULL
-#' for (i in 1:nrep) {
-#' z <- simu_deaths(time,prob,n)
-#' y <- c(y,z$Death[z$Event==1])
-#' }
-#' p <- sapply(time,function(i) sum(y==i))
-#' p <- cumsum(p)
-#' r <- lm(prob~p)
-#' par(mfcol=c(2,1),mar=c(3,4,3,2))
-#' plot(time,predict(r),xlab="Time",ylab="Survival",ylim=c(0,1))
+#' y <- replicate(nrep,simu_deaths(time,prob,n_ind)$Death)
+#' p <- sapply(0:99,function(i) sum(y==i))
+#' p <- p/max(p)
+#' plot(time[-100],p,xlab="Time",ylab="Survival")
 #' points(time,prob,type="l",lwd=2)
-#' plot(time,prob-predict(r),xlab="Time",ylab="Differences")
-#' par(mfcol=c(1,1))
 #'
 #' @export
-# 
-simu_deaths <- function(time,prob,n) {
+
+simu_deaths <- function(time,prob,n_ind) {
   
   # Random probabilities
-  pr <- runif(n, min=0, max=1)
+  pr <- stats::runif(n_ind, min=0, max=1)
   
   # Calculate in which interval they fall.
   x <- outer(prob,pr,">=")
@@ -68,8 +57,8 @@ simu_deaths <- function(time,prob,n) {
     df <- data.frame(Death=time[j],Event=1)
     
   # If there are no -1's (i.e. no deaths), it's even easier.
-  } else if (n0==n) {
-    df <- data.frame(Death=numeric(n),Event=0)
+  } else if (n0==n_ind) {
+    df <- data.frame(Death=numeric(n_ind),Event=0)
     
   # It's more complicated when there are 0's and -1's.
   } else {
